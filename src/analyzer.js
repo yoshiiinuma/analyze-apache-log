2d0a35dfb998188ea165519886d35c177e76c1e3
Yoshiaki Iinuma <yoshiaki.iinuma@hawaii.gov>
2018-09-17 14:08:18 -1000

    Removed old code

--- src/analyzer.js
+++ src/analyzer.js
@@ -76,49 +76,6 @@ export const analyze = (file, opts = {}) => {
   });
 }
 
-export const analyzeOld = (file, opts = {}) => {
-  const min = (opts.period == undefined)? 1 : parseInt(opts.period);
-  const timeEnd = minToTimeEndOffset(min);
-  const period = min * 60 * 1000;
-
-  return new Promise((resolve, reject) => {
-    let r = {};
-    let cur;
-    let time;
-    let timestamp = null;
-    let timeslot = null;
-
-    const rl = readline.createInterface({
-      input: fs.createReadStream(file),
-      terminal: false
-    });
-
-    rl.on('close', () => resolve(r));
-    rl.on('error', (e) => reject(e));
-    rl.on('line', (l) => {
-      const [ip, timeOrig, usr, name, req, cd, ref, agt, resSize,
-         inSize, outSize, elapsed] = l.split(' | ');
-
-      if (opts.ip && ip !== opts.ip) return;
-
-      time = timeOrig.slice(1, 21);
-      timestamp = Date.parse(time.slice(0, 11) + ' ' + time.slice(12, 21));
-
-      if (timeslot === null || timestamp >= timeslot + period) {
-        timeslot = timestamp - timestamp % 60000;
-        cur = time.slice(0, timeEnd);
-        r[cur] = { count: 0, ip: {} };
-      }
-      if (r[cur]['ip'][ip] == undefined) r[cur]['ip'][ip] = { count:0, requests: {} };
-      if (r[cur]['ip'][ip]['requests'][req] == undefined) r[cur]['ip'][ip]['requests'][req] = 0;
-
-      r[cur]['count']++;
-      r[cur]['ip'][ip]['count']++;
-      r[cur]['ip'][ip]['requests'][req]++;
-    });
-  });
-}
-
 const minToTimeEndOffset = (period) => {
   let min = period || 1;
   if (min < 1) min = 1;
